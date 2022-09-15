73cf675abe834f360138ce4978a36d1d622bb9b7
Bigbanj <toluwalasekingulot@gmail.com>
2022-09-14 21:55:21 -0700

    f

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,15 +7,14 @@
  *
  *    * Return: 0
  *
- *     */
+ */
 void print_alphabet(void)
 {
 		char c;
 
-
-			for (c = 'a'; c <= 'z'; c++)
-					{
-								_putchar(c);
-									}
-			_putchar('\n');
+		for (c = 'a'; c <= 'z'; c++)
+		{
+					_putchar(c);
+		}           
+	_putchar('\n');
 }
